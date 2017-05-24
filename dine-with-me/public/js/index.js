4b57f4d5967c6e5ee78a80edb6e9e0c1ac64cd81
Marie Schneider <mschneid@ucsd.edu>
2017-05-24 06:39:08 -0700

    fixed login, added push notifications

--- dine-with-me/public/js/index.js
+++ dine-with-me/public/js/index.js
@@ -1,3 +1,5 @@
+
+
 $("footer > tab").click(function () {
     $(this).addClass("active").siblings().removeClass("active");
     $("#" + $(this).attr("id") + "-section").addClass("active").siblings().removeClass("active");
